f1d73af9e85e8f5748c1929df97cdfcffca9ac0b
aev410 <115030325+aev410@users.noreply.github.com>
2023-05-08 20:51:02 +0200

    Update PracticaT17.py

--- PracticaT17.py
+++ PracticaT17.py
@@ -9,11 +9,11 @@ class Media:
         return int(input())
     
     def add(self, n):
-        self.arrayNumeros.append(n)
+        self.array_numeros.append(n)
     def media(self):
         suma = 0
-        for i in self.arrayNumeros:
+        for i in self.array_numeros:
             suma += i
-        m = suma/len(self.arrayNumeros)
+        m = suma/len(self.array_numeros)
         print("La media es "+str(m))
         return m
